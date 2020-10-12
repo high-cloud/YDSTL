8554c815258b494ba4abbed6847ba4f54ba84e1c
high-cloud <695397065@qq.com>
2020-10-12 23:04:15 +0800

    update algobase

--- ydstl_algobase.h
+++ ydstl_algobase.h
@@ -1,7 +1,7 @@
 #ifndef YDSTL_ALGOBASE_H
 #define YDSTL_ALGOBASE_H
 
-
+#include"ydstl_iterator.h"
 // 基本算法
 
 //-----------------equal-----------------------------------------
@@ -47,10 +47,112 @@ ForwardIterator fill_n(ForwardIterator first,Size n,const T& value){
     return first;
 }
 
+//--------------------------iter_swap--------------------
+// 交换两个iterneirong
+template<typename ForwardIterator1,typename ForwardIterator2>
+inline void iter_swap(ForwardIterator1 iter1,ForwardIterator2 iter2)
+{
+    __iter_swap(iter1,iter2,value_type(iter1));
+}
+
+
+template<typename ForwardIterator1,typename ForwardIterator2,typename T>
+inline void iter_swap(ForwardIterator1 iter1,ForwardIterator2 iter2,T*)
+{
+    T tmp=*iter1;
+    *iter1=*iter2;
+    *iter2=*iter1;
+}
+
+
+// ----------------------------------------字典排序----------------------------------
+//如果 第一个串小于或等于第二个串，返回true
+template<typename InputIterator1,typename InputIterator2>
+bool lexicongraphical_compare(InputIterator1 first1,InputIterator1 last1,
+                                InputIterator2 first2,InputIterator2 last2)
+                {
+                    for(; first1!=last1 && first2!=last2;++first1,++first2)
+                    {
+                        if(*first1<*first2)
+                        {
+                            return true;
+                        }
+                        else if(*first1>*first2)
+                        {
+                            return false;
+                        }
+                    }
+                    return first1==last1 && first2!=last2;
+                }
+
+
+template<typename InputIterator1,typename InputIterator2,typename Compare>
+bool lexicongraphical_compare(InputIterator1 first1,InputIterator1 last1,
+                                InputIterator2 first2,InputIterator2 last2,Compare compare)
+                {
+                    for(; first1!=last1 && first2!=last2;++first1,++first2)
+                    {
+                        if(compare(*first1,*first2))
+                        {
+                            return true;
+                        }
+                        else if(compare(*first2,*first1))
+                        {
+                            return false;
+                        }
+                    }
+                    return first1==last1 && first2!=last2;
+                }
+
+
+// ----------------------------max------------------------------
+//
+template<typename T>
+inline const T& max(const T&a,const &T b)
+{
+    return a<b? b:a;
+}
+
+
+template<typename T,typename Compare>
+inline const T& max(const T&a,const &T b,Compare comp)
+{
+    return comp(a,b)? b:a;
+}
+
+//--------------------------min----------------------------------
+//
+template<typename T>
+inline const T& min(const T& a,const T& b)
+{
+    return a<b? a :b;
+}
 
 
 
+template<typename T,typename Compare>
+inline const T& min(const T& a,const T& b,Compare comp)
+{
+    return comp(a,b)? a :b;
+}
 
+
+//--------------------------mismatch-------------------
+// 返回序列中第一个不同的迭代器，用户自行保证第二个序列不短于第一个序列
+//TODO (因为缺少 pair)
+
+
+//---------------------------swap------------------------------
+template<class T>
+inline void swap(T& a,T& b)
+{
+    T tmp=a;
+    a=b;
+    b=tmp;
+}
+
+
+//-----------------------copy-----------------------------
 
 
 #endif
--- ydstl_vector.h
+++ ydstl_vector.h
@@ -93,64 +93,7 @@ namespace ydstl
             destroy(end);
         }
 
-        void insert(iterator position, size_type n, const T &x)
-        {
-            if (n != 0)
-            {
-                if (end_of_storage - end >= n)
-                {
-                    //如果备用空间大于新增元素个数
-                    T x_copy = x;
-                    size_type elems_after = end - position;
-                    iterator old_end = end;
-                    if (elems_after > n)
-                    {
-                        uninitialized_copy(end - n, end, end);
-                        end += n;
-                        copy_backward(position, old_end - n, old_end);
-                        fill(position, position + n, x_copy);
-                    }
-                    else
-                    {
-                        uninitialized_fill_n(end, n - elems_after, x_copy);
-                        end += n - elems_after;
-                        uninitialized_copy(position, old_end, end);
-                        end += elems_after;
-                        fill(position, old_end, x_copy);
-                    }
-                }
-                else
-                {
-                    //备用空间小于添加元素数量
-
-                    const size_type old_size = size();
-                    const size_type len = old_size+max(old_size,n);
-
-                    iterator new_start = data_allocator::allocate(len);
-                    iterator new_end = new_start;
-
-                    try{
-                        new_end=uninitialized_copy(start,position,new_start);
-                        new_end=uninitialized_fill_n(new_end,n,x);
-                        new_end=uninitialized_copy(position,end,new_end);
-                    }
-                    catch(...)
-                    {
-                        destroy(new_start,new_end);
-                        data_allocator::deallocate(new_start,len);
-                        throw;
-                    }
-
-                    //清除原有空间
-                    destroy(start，end);
-                    deallocate();
-
-                    start=new_start;
-                    end=new_end;
-                    end_of_storage=start+len;
-                }
-            }
-        }
+        void insert(iterator position, size_type n, const T &x);
 
         // 清除某位置的元素
         iterator erase(iterator position)
@@ -188,38 +131,103 @@ namespace ydstl
         // helper function
         //
         void insert_aux(iterator position, const T &x)
-        {
-            const size_type old_size = size();
-            const size_type len = old_size == 0 ? 1 : 2 * old_size;
-
-            iterator new_start = data_allocator::allocate(len);
-            iterator new_end = new_start;
-
-            try
-            {
-                // 拷贝前面的内容
-                new_end = uninitialized_copy(start, position, new_start);
-                // 添加新元素
-                construct(new_end, x);
-                ++new_end;
-                new_end = uninitialized_copy(position, end, new_end);
-            }
-            catch (...)
-            {
-                // commit or rollback
-                destroy(new_start, new_end);
-                data_allocator::deallocate(new_start, len);
-                throw;
-            }
-
-            destroy(start, end);
-            deallocate();
-
-            start = new_start;
-            end = new_end;
-            end_of_storage = new_start + len;
-        }
+      
     };
+
+    template <typename T, typename Alloc>
+    void vector<T, Alloc>::insert_aux(iterator position, const T &x)
+    {
+        const size_type old_size = size();
+        const size_type len = old_size == 0 ? 1 : 2 * old_size;
+
+        iterator new_start = data_allocator::allocate(len);
+        iterator new_end = new_start;
+
+        try
+        {
+            // 拷贝前面的内容
+            new_end = uninitialized_copy(start, position, new_start);
+            // 添加新元素
+            construct(new_end, x);
+            ++new_end;
+            new_end = uninitialized_copy(position, end, new_end);
+        }
+        catch (...)
+        {
+            // commit or rollback
+            destroy(new_start, new_end);
+            data_allocator::deallocate(new_start, len);
+            throw;
+        }
+
+        destroy(start, end);
+        deallocate();
+
+        start = new_start;
+        end = new_end;
+        end_of_storage = new_start + len;
+    }
+
+    template <typename T, typename Alloc>
+    void vector<T, Alloc>::insert(iterator position, size_type n, const T &x)
+    {
+        if (n != 0)
+        {
+            if (end_of_storage - end >= n)
+            {
+                //如果备用空间大于新增元素个数
+                T x_copy = x;
+                size_type elems_after = end - position;
+                iterator old_end = end;
+                if (elems_after > n)
+                {
+                    uninitialized_copy(end - n, end, end);
+                    end += n;
+                    copy_backward(position, old_end - n, old_end);
+                    fill(position, position + n, x_copy);
+                }
+                else
+                {
+                    uninitialized_fill_n(end, n - elems_after, x_copy);
+                    end += n - elems_after;
+                    uninitialized_copy(position, old_end, end);
+                    end += elems_after;
+                    fill(position, old_end, x_copy);
+                }
+            }
+            else
+            {
+                //备用空间小于添加元素数量
+
+                const size_type old_size = size();
+                const size_type len = old_size + max(old_size, n);
+
+                iterator new_start = data_allocator::allocate(len);
+                iterator new_end = new_start;
+
+                try
+                {
+                    new_end = uninitialized_copy(start, position, new_start);
+                    new_end = uninitialized_fill_n(new_end, n, x);
+                    new_end = uninitialized_copy(position, end, new_end);
+                }
+                catch (...)
+                {
+                    destroy(new_start, new_end);
+                    data_allocator::deallocate(new_start, len);
+                    throw;
+                }
+
+                //清除原有空间
+                destroy(start，end);
+                deallocate();
+
+                start = new_start;
+                end = new_end;
+                end_of_storage = start + len;
+            }
+        }
+    }
 
 } // namespace ydstl
 
